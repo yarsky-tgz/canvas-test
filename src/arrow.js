1ab75a756a9e5a439ab03ff0d6052a60c616b202
Yaroslav Dobzhanskij <merlyn.tgz@gmail.com>
2017-02-10 22:23:02 +0200

    fabric.Arrow rewritten

--- src/arrow.js
+++ src/arrow.js
@@ -1,67 +1,41 @@
-(function() {
-  fabric.Arrow = fabric.util.createClass(fabric.Line, {
+(function () {
+  'use strict';
+  var pass = function () {};
+  fabric.Arrow = fabric.util.createClass(fabric.Polygon, {
     type: 'arrow',
     arrowLength: 0,
     arrowAngle: 0,
-    initialize: function(points, options) {
-      options = options || {};
-      this.callSuper('initialize', points, options);
+    initialize: function(linePoints, options) {
+      var points = [
+        {x: linePoints[0], y: linePoints[1]},
+        {x: linePoints[2], y: linePoints[3]}
+      ];
       this.set('arrowLength', options.arrowLength || 10);
       this.set('arrowAngle', options.arrowAngle || 45);
-    },
-    _render: function(ctx, noTransform) {
-      this.callSuper('_render', ctx, noTransform);
-      ctx.beginPath();
-      if (noTransform) {
-        var cp = this.getCenterPoint();
-        ctx.translate(
-          cp.x - this.strokeWidth / 2,
-          cp.y - this.strokeWidth / 2
-        );
-      }
-      var p = this.calcLinePoints();
-      var arrowPoints = this._getArrowPoints();
-      ctx.moveTo(arrowPoints.left.x, arrowPoints.left.y);
-      ctx.lineTo(p.x2, p.y2);
-      ctx.lineTo(arrowPoints.right.x, arrowPoints.right.y);
-      ctx.lineWidth = this.strokeWidth;
-      var origStrokeStyle = ctx.strokeStyle;
-      ctx.strokeStyle = this.stroke || ctx.fillStyle;
-      this.stroke && this._renderStroke(ctx);
-      ctx.strokeStyle = origStrokeStyle;
-    },
-    _getNonTransformedDimensions: function() {
-      var p = this.calcLinePoints();
-      var ap = this._getArrowPoints();
-      var coords = {
-        x: [p.x1, p.x2, ap.left.x, ap.right.x],
-        y: [p.y1, p.y2, ap.left.y, ap.right.y]
-      };
-      var minX = fabric.util.array.min(coords.x);
-      var maxX = fabric.util.array.max(coords.x);
-      var minY = fabric.util.array.min(coords.y);
-      var maxY = fabric.util.array.max(coords.y);
-      return {
-        x: maxX - minX + this.strokeWidth,
-        y: maxY - minY + this.strokeWidth
-      }
-    },
-    _getArrowPoints: function() {
-      var p = this.calcLinePoints();
       var arrowRadAngle = this.arrowAngle * Math.PI / 180;
-      var lineAngle = Math.atan2(p.y1 - p.y2, p.x1 - p.x2);
+      var lineAngle = Math.atan2(points[0].y - points[1].y, points[0].x - points[1].x);
       var leftAngle = lineAngle - arrowRadAngle;
       var rightAngle = lineAngle + arrowRadAngle;
-      return {
-        left: {
-          x: this._getArrowOffset(p.x2, leftAngle, Math.cos),
-          y: this._getArrowOffset(p.y2, leftAngle, Math.sin)
-        },
-        right: {
-          x: this._getArrowOffset(p.x2, rightAngle, Math.cos),
-          y: this._getArrowOffset(p.y2, rightAngle, Math.sin),
-        }
-      };
+      points.push({
+        x: this._getArrowOffset(points[1].x, leftAngle, Math.cos),
+        y: this._getArrowOffset(points[1].y, leftAngle, Math.sin)
+      });
+      points.push(points[1]);
+      points.push({
+        x: this._getArrowOffset(points[1].x, rightAngle, Math.cos),
+        y: this._getArrowOffset(points[1].y, rightAngle, Math.sin)
+      });
+      this.callSuper('initialize', points, options);
+    },
+    commonRender: function(ctx, noTransform) {
+      var allPoints = this.points.concat();
+      var arrowPoints = this.points.splice(2, 3);
+      this.callSuper('commonRender', ctx, noTransform);
+      this.points = arrowPoints;
+      this.callSuper('commonRender', {moveTo: ctx.moveTo.bind(ctx), lineTo: ctx.lineTo.bind(ctx), beginPath: pass}, noTransform);
+      this.points = allPoints;
+      this._renderStroke(ctx);
+      return false;
     },
     _getArrowOffset: function (position, angle, method) {
       return position + Math.round(this.arrowLength * method(angle));
